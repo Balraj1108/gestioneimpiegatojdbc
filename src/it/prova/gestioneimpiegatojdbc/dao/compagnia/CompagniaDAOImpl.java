6cb09ab5f488c2d7f532dafec0636828f9d283f8
Balraj1108 <balrajsingh1108@gmail.com>
2022-10-21 00:15:52 +0200

    fine metodi compagnia

--- src/it/prova/gestioneimpiegatojdbc/dao/compagnia/CompagniaDAOImpl.java
+++ src/it/prova/gestioneimpiegatojdbc/dao/compagnia/CompagniaDAOImpl.java
@@ -1,6 +1,7 @@
 package it.prova.gestioneimpiegatojdbc.dao.compagnia;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -10,6 +11,8 @@ import java.util.List;
 import it.prova.gestioneimpiegatojdbc.dao.AbstractMySQLDAO;
 import it.prova.gestioneimpiegatojdbc.model.Compagnia;
 import it.prova.gestioneimpiegatojdbc.model.Impiegato;
+
+
 
 public class CompagniaDAOImpl extends AbstractMySQLDAO implements CompagniaDAO {
 
@@ -49,47 +52,220 @@ public class CompagniaDAOImpl extends AbstractMySQLDAO implements CompagniaDAO {
 
 	@Override
 	public Compagnia get(Long idInput) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (idInput == null || idInput < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		Compagnia result = null;
+		try (PreparedStatement ps = connection.prepareStatement("select * from compagnia where id=?")) {
+
+			ps.setLong(1, idInput);
+			try (ResultSet rs = ps.executeQuery()) {
+				if (rs.next()) {
+					result = new Compagnia();
+					result.setRagioneSociale(rs.getString("ragionesociale"));
+					result.setFatturatoAnnuo(rs.getInt("fatturatoannuo"));
+					result.setDataFondazione(rs.getDate("datafondazione"));
+					result.setId(rs.getLong("ID"));
+				} else {
+					result = null;
+				}
+			} // niente catch qui
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int update(Compagnia input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (input == null || input.getId() == null || input.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (PreparedStatement ps = connection.prepareStatement(
+				"UPDATE compagnia SET ragionesociale=?, fatturatoannuo=?, datafondazione=? where id=?;")) {
+			ps.setString(1, input.getRagioneSociale());
+			ps.setInt(2, input.getFatturatoAnnuo());
+			// quando si fa il setDate serve un tipo java.sql.Date
+			ps.setDate(3, new java.sql.Date(input.getDataFondazione().getTime()));
+			ps.setLong(4, input.getId());
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int insert(Compagnia input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (input == null)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (PreparedStatement ps = connection.prepareStatement(
+				"INSERT INTO compagnia (ragionesociale, fatturatoannuo, datafondazione) VALUES (?, ?, ?);")) {
+			ps.setString(1, input.getRagioneSociale());
+			ps.setInt(2, input.getFatturatoAnnuo());
+			// quando si fa il setDate serve un tipo java.sql.Date
+			ps.setDate(3, new java.sql.Date(input.getDataFondazione().getTime()));
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int delete(Compagnia input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (input == null || input.getId() == null || input.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (PreparedStatement ps = connection.prepareStatement("DELETE FROM compagnia WHERE ID=?")) {
+			ps.setLong(1, input.getId());
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public List<Compagnia> findByExample(Compagnia input) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+		
+		ArrayList<Compagnia> result = new ArrayList<Compagnia>();
+		Compagnia userTemp = null;
+		
+		try (Statement s = connection.createStatement()){
+			
+			String query ="select * from compagnia where ";
+			
+			if (!(input.getRagioneSociale() == null)) {
+				query += "ragionesociale like '" + input.getRagioneSociale() + "%' and ";
+			}
+			if (!(input.getFatturatoAnnuo() < 0)) {
+				query += "fatturatoannuo like '" + input.getFatturatoAnnuo() + "%' and ";
+			}
+			if (!(input.getDataFondazione() == null)) {
+				query += "datecreated > '" + new java.sql.Date(input.getDataFondazione().getTime()) + " ' and ";
+			}
+			query+= "1=1";
+			System.out.println(query);
+			
+			try (ResultSet rs = s.executeQuery(query);) {
+				while (rs.next()) {
+					userTemp = new Compagnia();
+					userTemp.setRagioneSociale(rs.getString("ragionesociale"));
+					userTemp.setFatturatoAnnuo(rs.getInt("fatturatoannuo"));
+					userTemp.setDataFondazione(rs.getDate("datafondazione"));
+					userTemp.setId(rs.getLong("ID"));
+					result.add(userTemp);
+				}
+			} // niente catch qui			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			throw e;
+		}
+		
+		return result;
 	}
 
 	@Override
 	public List<Impiegato> findAllByDataAssunzioneMaggioreDi(Date dateCreatedInput) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (dateCreatedInput == null)
+			throw new Exception("Valore di input non ammesso.");
+
+		ArrayList<Impiegato> result = new ArrayList<Impiegato>();
+		Impiegato userTemp = null;
+		Compagnia compTemp = null;
+
+		try (PreparedStatement ps = connection.prepareStatement("select * from impiegato i inner join compagnia c on c.id=i.compagnia_id where i.dataassunzione > ?;")) {
+			// quando si fa il setDate serve un tipo java.sql.Date
+			ps.setDate(1, new java.sql.Date(dateCreatedInput.getTime()));
+
+			try (ResultSet rs = ps.executeQuery();) {
+				while (rs.next()) {
+					userTemp = new Impiegato();
+					userTemp.setNome(rs.getString("NOME"));
+					userTemp.setCognome(rs.getString("COGNOME"));
+					userTemp.setCodiceFiscale(rs.getString("codicefiscale"));
+					userTemp.setDataNascita(rs.getDate("datanascita"));
+					userTemp.setDataAssunzione(rs.getDate("dataassunzione"));
+					userTemp.setId(rs.getLong("i.ID"));
+					
+					compTemp = new Compagnia();
+					compTemp.setId(rs.getLong("c.id"));
+					compTemp.setRagioneSociale(rs.getString("ragionesociale"));
+					compTemp.setFatturatoAnnuo(rs.getInt("fatturatoannuo"));
+					compTemp.setDataFondazione(rs.getDate("datafondazione"));
+					userTemp.setCompagnia(compTemp);
+					result.add(userTemp);
+				}
+			} // niente catch qui
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public List<Compagnia> findAllByRagioneSocialeContiene(String ragioneSocialeInput) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+		if (ragioneSocialeInput.equals("") || ragioneSocialeInput == null) {
+			throw new Exception("Valore di input non ammesso.");
+		}
+		
+		ArrayList<Compagnia> result = new ArrayList<Compagnia>();
+		Compagnia userTemp = null;
+		
+		try (PreparedStatement ps = connection.prepareStatement("select * from compagnia c where c.ragionesociale = ?;")) {
+			ps.setString(1, ragioneSocialeInput);
+			
+			try (ResultSet rs = ps.executeQuery();) {
+				while (rs.next()) {
+					userTemp = new Compagnia();
+					userTemp.setRagioneSociale(rs.getString("ragionesociale"));
+					userTemp.setFatturatoAnnuo(rs.getInt("fatturatoannuo"));
+					userTemp.setDataFondazione(rs.getDate("datafondazione"));
+					userTemp.setId(rs.getLong("ID"));
+					result.add(userTemp);
+				}
+			} // niente catch qui
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+			// TODO: handle exception
+		}
+		return result;
 	}
 	
-	
-	
-
 }
--- src/it/prova/gestioneimpiegatojdbc/test/TestGestioneImpiegatoJdbc.java
+++ src/it/prova/gestioneimpiegatojdbc/test/TestGestioneImpiegatoJdbc.java
@@ -1,6 +1,8 @@
 package it.prova.gestioneimpiegatojdbc.test;
 
 import java.sql.Connection;
+import java.util.Date;
+import java.util.List;
 
 import it.prova.gestioneimpiegatojdbc.connection.MyConnection;
 import it.prova.gestioneimpiegatojdbc.dao.Constants;
@@ -8,6 +10,8 @@ import it.prova.gestioneimpiegatojdbc.dao.compagnia.CompagniaDAO;
 import it.prova.gestioneimpiegatojdbc.dao.compagnia.CompagniaDAOImpl;
 import it.prova.gestioneimpiegatojdbc.dao.impiegato.ImpiegatoDAO;
 import it.prova.gestioneimpiegatojdbc.dao.impiegato.ImpiegatoDAOImpl;
+import it.prova.gestioneimpiegatojdbc.model.Compagnia;
+
 
 public class TestGestioneImpiegatoJdbc {
 
@@ -23,14 +27,77 @@ public class TestGestioneImpiegatoJdbc {
 			compagniaDAOInstance = new CompagniaDAOImpl(connection);
 
 			//System.out.println(compagniaDAOInstance.list());
-
+			
+			testFindByIdCompagnia(compagniaDAOInstance);
+			
+			testInsertCompagnia(compagniaDAOInstance);
+			
+			testUpdateCompagnia(compagniaDAOInstance);
+			
+			System.out.println(compagniaDAOInstance.findAllByRagioneSocialeContiene("ConadSLR"));
+			
+			
 			
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}		
+	}
+	
+	private static void testFindByIdCompagnia(CompagniaDAO compagniaDAOInstance) throws Exception {
+		System.out.println(".......testFindByIdCompagnia inizio.............");
+		List<Compagnia> elencoVociPresenti = compagniaDAOInstance.list();
+		if (elencoVociPresenti.size() < 1)
+			throw new RuntimeException("testFindById : FAILED, non ci sono voci sul DB");
+
+		Compagnia primoDellaLista = elencoVociPresenti.get(0);
+
+		Compagnia elementoCheRicercoColDAO = compagniaDAOInstance.get(primoDellaLista.getId());
+		if (elementoCheRicercoColDAO == null || !elementoCheRicercoColDAO.getRagioneSociale().equals(primoDellaLista.getRagioneSociale()))
+			throw new RuntimeException("testFindById : FAILED, le login non corrispondono");
+
+		System.out.println(".......testFindByIdCompagnia fine: PASSED.............");
+	}
+	
+	private static void testInsertCompagnia(CompagniaDAO compagniaDAOInstance) throws Exception {
+		System.out.println(".......testInsertCompagnia inizio.............");
+		int quantiElementiInseriti = compagniaDAOInstance
+				.insert(new Compagnia("AcerSRL", 15000, new Date()));
+		if (quantiElementiInseriti < 1)
+			throw new RuntimeException("testInsertUser : FAILED");
+
+		System.out.println(".......testInsertCompagnia fine: PASSED.............");
+	}
+	
+	private static void testUpdateCompagnia(CompagniaDAO compagniaDAOInstance) throws Exception {
+		System.out.println(".......testUpdateCompagnia inizio.............");
+		List<Compagnia> elencoNegoziPresenti = compagniaDAOInstance.list();
 		
-		
+		if (!elencoNegoziPresenti.isEmpty()) {
+			
+			Compagnia primoNegozioDellaLista = elencoNegoziPresenti.get(0);
+			String cambioNomeNegozio = "COOP-SRL";
+			System.out.println("Elemento da modificare: " + primoNegozioDellaLista);
+			primoNegozioDellaLista.setRagioneSociale(cambioNomeNegozio);
+			
+			compagniaDAOInstance.update(primoNegozioDellaLista);
+			
+			// test find by id E verifico se l'update è andato a buon fine
+			Long idPerTestUpdate = primoNegozioDellaLista.getId();
+			Compagnia negozioTrovatoConId = compagniaDAOInstance.get(idPerTestUpdate);
+
+			System.out.println("Elemento modificato: " + negozioTrovatoConId);
+
+			if (negozioTrovatoConId == null || !negozioTrovatoConId.getRagioneSociale().equals(cambioNomeNegozio))
+				throw new RuntimeException("Test fallitto: update non avvenuto");			
+		}
+		else {
+			throw new RuntimeException("Test fallito: database vuoto");
+		}
+		System.out.println(".......testUpdateCompagnia fine: PASSED.............");
 		
 	}
+	
+	
+	
 }
